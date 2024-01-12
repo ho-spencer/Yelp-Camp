53ba38bf5660490c9ec5bc65e04a7a47a9ad06b4
Spencer Ho <bspencer08@cox.net>
2024-01-11 17:38:59 -0800

    Yelp Camp - Center Map and Add Marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,6 +1,12 @@
+// Enable Map
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: "map",       // container ID -- must match the "id" of the div where we are displaying the map on the page
-    center: [-74.5, 40],    // starting position [lng, lat]
-    zoom: 9,                // starting zoom level
+    container: "map",                               // container ID -- must match the "id" of the div where we are displaying the map on the page
+    center:  campground.geometry.coordinates,       // starting position [lng, lat] - coordinates from geocoding
+    zoom: 10,                                        // starting zoom level
 });
+
+// Add Marker
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
